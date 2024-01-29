5503bbae921f76e129d5a03592c75a323dae83e6
William Smith <williamsmithe@icloud.com>
2024-01-29 08:57:58 -0800

    Add project files.

---
+++ SQLServerForExcel/IntelliSenseAddIn.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+namespace SQLServerForExcel
+{
+    public class IntelliSenseAddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            IntelliSenseServer.Install();
+        }
+        public void AutoClose()
+        {
+            IntelliSenseServer.Uninstall();
+        }
+    }
+}
---
+++ SQLServerForExcel/Select.cs
@@ -0,0 +1,28 @@
+﻿using ExcelDna.Integration;
+using System.Data.SqlClient;
+
+namespace SQLServerForExcel
+{
+    public class Select
+    {
+        [ExcelFunction(IsThreadSafe = true, Description = "Select Scalar")]
+        public static object SelectScalar(string sql)
+        {
+            string connectionString = "Data Source=DESKTOP-8M9VJFP\\SQLEXPRESS;Initial Catalog=PowerBI;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create SqlCommand
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+
+                    var result = command.ExecuteScalar();
+
+                    if (result is null) return ExcelError.ExcelErrorValue;
+                    else return result.ToString() ?? string.Empty;
+                }
+            }
+        }
+    }
+}
